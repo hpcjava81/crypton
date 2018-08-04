4aee36a2de9c057e2022b0403b7be7b35c8c8ad0
garuna81 <ruhela@gmail.com>
2018-08-04 20:16:15 +0100

    Add the missing Instrument class

---
+++ src/main/java/com/hpcjava81/crypton/domain/Instrument.java
@@ -0,0 +1,32 @@
+package com.hpcjava81.crypton.domain;
+
+/**
+ * Represents a symbol, for example, ETH-USD
+ */
+public class Instrument {
+
+    private final String name;
+
+    public Instrument(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Instrument that = (Instrument) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
