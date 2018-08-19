4a021052115f85ee8b9f0cb54d905a480521257a
garuna81 <ruhela@gmail.com>
2018-08-19 10:24:58 +0100

    Remove unused TArb

--- src/main/java/com/hpcjava81/crypton/algo/arb/TArb.java
+++ src/main/java/com/hpcjava81/crypton/algo/arb/TArb.java
@@ -3,10 +3,10 @@ package com.hpcjava81.crypton.algo.arb;
 import com.hpcjava81.crypton.algo.Algo;
 
 public class TArb implements Algo {
-    private static final double TOL = 1e-2;
 
-    public static boolean runAlgo(double px1, double px2, double px3) {
-        return Math.abs(px1/px2 - px3) > TOL;
+    public static boolean runAlgo() {
+        //TODO do we need this abstraction?
+        return false;
     }
 
 }
